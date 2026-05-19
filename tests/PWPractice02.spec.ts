f976e9ad0545a2c470a76458018c80cad69166fd
ShreyB <shreypractice.github@gmail.com>
2026-05-19 18:07:17 +0530

    Updates_1905_18:08

--- tests/PWPractice02.spec.ts
+++ tests/PWPractice02.spec.ts
@@ -1,1 +1,9 @@
-import {page,expect} from "@playwright/test"
+import {test,expect} from "@playwright/test"
+
+test("Verify your URL",async({page})=>
+{
+    await page.goto("https://bookcart.azurewebsites.net/");
+    let url:string=await page.url();
+    console.log("URL is:",url);
+    await expect(page).toHaveURL("https://bookcart.azurewebsites.net/")
+})
