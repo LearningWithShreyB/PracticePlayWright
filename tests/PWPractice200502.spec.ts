d632822af790bf4096958a9bbbeb65b5dd53d2f1
ShreyB <shreypractice.github@gmail.com>
2026-05-20 17:32:54 +0530

    Updates_2005_17:33

--- tests/PWPractice200502.spec.ts
+++ tests/PWPractice200502.spec.ts
@@ -2,8 +2,10 @@ import { test, expect, Locator } from "@playwright/test";
 
 test("Verify getByRole() Locators", async ({ page }) => {
     await page.goto("http://127.0.0.1:5500/tests/app.html");
-   /*  const textbox: Locator = page.getByRole("textbox", { name: "Username" });
-    await expect(textbox).toBeVisible(); */
+    /*  const textbox: Locator = page.getByRole("textbox", { name: "Username" });
+     await expect(textbox).toBeVisible(); */
 
     await expect(page.getByRole("textbox", { name: "Username" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "Primary Action" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "Toggle Button" })).toBeVisible();
 });
