02fb90a973494ae1e4f9fd402361dc40e9b74ed9
ShreyB <shreypractice.github@gmail.com>
2026-05-20 17:45:40 +0530

    Updates_2005_17:46

--- tests/PWPractice200502.spec.ts
+++ tests/PWPractice200502.spec.ts
@@ -8,4 +8,9 @@ test("Verify getByRole() Locators", async ({ page }) => {
     await expect(page.getByRole("textbox", { name: "Username" })).toBeVisible();
     await expect(page.getByRole("button", { name: "Primary Action" })).toBeVisible();
     await expect(page.getByRole("button", { name: "Toggle Button" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "Div with button role" })).toBeVisible();
+    await expect(page.getByRole("checkbox", { name: "Accept terms" })).toBeVisible();
+    await expect(page.getByRole("link", { name: "Home" }).first()).toBeVisible();
+    await expect(page.getByRole("link", { name: "Products" }).first()).toBeVisible();
+    await expect(page.getByRole("link", { name: "Contact" }).first()).toBeVisible();
 });
