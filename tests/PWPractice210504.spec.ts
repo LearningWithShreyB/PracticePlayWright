59cb0ad9d76e99b63c8f8a5cfc1d89daab5514f0
ShreyB <shreypractice.github@gmail.com>
2026-05-21 19:03:36 +0530

    Updates_2105_19:04

--- tests/PWPractice210504.spec.ts
+++ tests/PWPractice210504.spec.ts
@@ -6,4 +6,7 @@ test('CSS Locators Practice02', async ({page})=>
     const para1: Locator = page.locator('p:nth-child(1)');
     await expect(para1).toBeVisible();
     await expect(para1).toHaveText("Welcome to the new Tricentis store!");
+    const para2: Locator = page.locator('p:nth-child(2)');
+    await expect(para2).toBeVisible();
+    await expect(para2).toHaveText("Feel free to shop around and explore everything.");
 })
