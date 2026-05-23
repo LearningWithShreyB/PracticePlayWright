1d656c7a8e6430a6c84258aeef0b7492e203c21e
ShreyB <shreypractice.github@gmail.com>
2026-05-23 23:48:25 +0530

    Updates_2305_23:49

--- tests/PWPractice240501.spec.ts
+++ tests/PWPractice240501.spec.ts
@@ -7,5 +7,51 @@ test("Verify XPath Locators", async ({page})=>
     // 1. Absolute XPath (Full XPath)
     const logo:Locator=page.locator('//body/div[4]/div[1]/div[1]/div[1]/a/img');
     await expect(logo).toBeVisible();
+
+    // 2. Relative XPath (Partial XPath)
+    const logo1:Locator=await page.locator("//*[@alt='Tricentis Demo Web Shop']");
+    await expect(logo1).toBeVisible();
+
+    // 3. XPath with contains() 
+    const tag2: Locator = page.locator('//h2//a[contains(@href, "computer")]');
+    await expect(tag2).toHaveCount(4);
+
+     // 4. XPath with starts-with()
+    const tag3:Locator=page.locator("//h2//a[starts-with(@href,'/build')]");
+    const totalCount:number=await tag3.count();
+    expect(totalCount).toBeGreaterThan(0);
+    await expect(tag3).toHaveCount(3);
+
+    /*
+    // Products ending with "/build" in href attribute
+    const tag4:Locator=page.locator("h2>a[href$='op']");
+    await expect(tag4).toHaveText('14.1-inch Laptop');
+
+
+    // Login link using CSS selector with exact text match
+
+    await expect(page.locator("a[href='/login']")).toBeVisible(); 
+
+    // Getting multiple elements handling
+    const tag5:Locator=page.locator("[style='font-size:100%;'][href$='rel']");
+    //const tag5:Locator=page.locator("a:not([style='font-size:100%;'])[href$='rel']");
+    //const tag5 = page.locator("a[href$='rel']:not([style='font-size:100%;'])");
+    //const text:string|null=await tag5.textContent();
+    //console.log("Value of element:",text);
+    await expect(tag5).toBeVisible();
+
+
+    const tag6:Locator=page.locator("[style='font-size:100%;']");
+    const totalElements:string[]=await tag6.allTextContents();
     
+    for(let value of totalElements)
+    {
+        console.log(value);
+    }
+
+    await expect(tag6.first()).toHaveText("apparel");
+    await expect(tag6.last()).toHaveText("computer");
+
+        */
+
 })
