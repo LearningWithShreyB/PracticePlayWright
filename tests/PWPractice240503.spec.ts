6d83cfd98100d0168d3c37dadf150247c0d99b75
ShreyB <shreypractice.github@gmail.com>
2026-05-24 00:55:11 +0530

    Updates_2405_00:55

---
+++ tests/PWPractice240503.spec.ts
@@ -0,0 +1,41 @@
+import {test, expect, Locator} from "@playwright/test";
+
+test("Verify XPath Locators", async ({page})=>
+{
+    await page.goto("https://www.w3schools.com/html/html_tables.asp");
+
+    // 1. self axis - Select <td> element that contains "mexico"
+    const tag1:Locator=page.locator("//td[text()='Mexico']/self::td");
+    await expect(tag1).toHaveText('Mexico');
+
+    // 2. parent axis - Get parent <tr> of the "Mexico" cell
+    const tag11:Locator=await page.locator("//td[text()='Mexico']/parent::tr");
+    await expect(tag11).toContainText("Francisco Chang");
+/*
+    // 3. XPath with contains() 
+    const tag2: Locator = page.locator('//h2//a[contains(@href, "computer")]');
+    await expect(tag2).toHaveCount(4);
+
+     // 4. XPath with starts-with()
+    const tag3:Locator=page.locator("//h2//a[starts-with(@href,'/build')]");
+    const totalCount:number=await tag3.count();
+    expect(totalCount).toBeGreaterThan(0);
+    await expect(tag3).toHaveCount(3);
+
+    // 5. XPath with last()
+
+    const tag4:Locator=page.locator("(//h2[@class='product-title']/a)[last()]");
+    await expect(tag4).toHaveText('Simple Computer');
+
+    // 6. XPath with text()
+
+    await expect(page.locator("//a[text()='Log in']")).toBeVisible(); 
+
+    // 7. XPath with position()
+    const tag5:Locator=page.locator("//div[@class='column follow-us']//li[position()='3']");
+    await expect(tag5).toHaveText("RSS");
+
+    const tag6:string=await page.locator("//div[@class='column follow-us']//li[position()='3']").innerText();
+    expect(tag6).toBe('RSS');
+ */
+})
