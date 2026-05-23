e8724c62a536cfba33ebd36fdb1e061a62789f65
ShreyB <shreypractice.github@gmail.com>
2026-05-23 17:42:45 +0530

    Updates_2305_17:43

--- tests/PWPractice230501.spec.ts
+++ tests/PWPractice230501.spec.ts
@@ -11,8 +11,24 @@ test("Verify CSS Locators", async ({ page }) => {
 
 
     // tag.class
-     const classtag: Locator = page.locator(".search-box-text");
+    const classtag: Locator = page.locator(".search-box-text");
     await classtag.fill("T-Shirts");
     await expect(classtag).toBeVisible();
     await page.waitForTimeout(2000);
+
+    // tag[attribute=value]
+    page.goto("https://www.google.com/");
+    const tag3: Locator = page.locator("[role='img']");
+    await expect(tag3).toBeVisible();
+    await page.waitForTimeout(2000);
+
+    //tag.class[attribute=value]
+    await expect(page.locator(".gb_4[aria-label='Search for Images ']")).toBeVisible();
+    await page.waitForTimeout(2000);
+
+    const tag4: Locator = page.locator(".gLFyf[title='Search']");
+    await tag4.fill("Playwright");
+    await expect(tag4).toBeVisible();
+    await page.waitForTimeout(2000);
+
 })
