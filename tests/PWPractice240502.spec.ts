064ea06f35243c2fd54ef49241e8451db2241f4e
ShreyB <shreypractice.github@gmail.com>
2026-05-24 00:40:55 +0530

    Updates_2405_00:41

--- tests/PWPractice240502.spec.ts
+++ tests/PWPractice240502.spec.ts
@@ -4,13 +4,27 @@ test("Verify XPath Locators", async ({page})=>
 {
     await page.goto("https://testautomationpractice.blogspot.com/");
 
-    //Handle Dynamic Elemnets
+    /* //Handle Dynamic Elemnets- By Xpath
     for(let i:number=1;i<=5;i++)
     {
         const button:Locator=page.locator("//button[text()='START' or text()='STOP']");
         await button.click();
         await page.waitForTimeout(3000);
+    } */
+
+
+    //Handle Dynamic Elemnets- By PW Locators
+
+    for(let i:number=1;i<=5;i++)
+    {
+        const button1:Locator=page.getByRole("button",{name: /START|STOP/ });
+        await button1.click();
+        await page.waitForTimeout(3000);
     }
+    
+
+
+
 
 
 
