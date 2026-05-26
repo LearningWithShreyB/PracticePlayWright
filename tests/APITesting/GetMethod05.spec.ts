01a10203e45a314173a7cb122d8c42f5d697d3aa
ShreyB <shreypractice.github@gmail.com>
2026-05-26 23:59:34 +0530

    Updates_2605_23:59

--- tests/APITesting/GetMethod05.spec.ts
+++ tests/APITesting/GetMethod05.spec.ts
@@ -1,3 +1,32 @@
-import {test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-test('Verifying the GET Method through Query Params',())
+test('Verifying the GET Method through Query Params', async ({ request }) => {
+    const firstname = "Jim";
+    const lastname = "Brown";
+
+    const getResponse = await request.get('/booking',
+        {
+            params:
+            {
+                firstname,
+                lastname
+            }
+        }
+    );
+
+    const getResponseBody = await getResponse.json();
+    console.log(getResponseBody);
+
+    expect(getResponse.ok()).toBeTruthy();
+    expect(getResponse.status()).toBe(200);
+
+    expect(getResponseBody.length).toBeGreaterThan(0);
+
+    for (const item of getResponseBody) {
+        expect(item).toHaveProperty("bookingid");
+        expect(typeof item.bookingid).toBe("number");
+        expect(item.bookingid).toBeGreaterThan(0);
+    }
+
+}
+);
