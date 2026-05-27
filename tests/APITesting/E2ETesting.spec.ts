c509968b52d5a0fc2b09f7a0634363c6b66e0192
ShreyB <shreypractice.github@gmail.com>
2026-05-27 19:04:04 +0530

    Updates_2705_19:05

--- tests/APITesting/E2ETesting.spec.ts
+++ tests/APITesting/E2ETesting.spec.ts
@@ -1,47 +1,60 @@
-import {test,expect} from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import tokenSourceRequestBody from '../../testdata/token_request_body.json';
 import postSourceRequestBody from '../../testdata/post_request_body.json';
 import putSourceRequestBody from '../../testdata/put_request_body.json';
 
-test('Verifying E2E testing of CRUD operations',async ({request})=>
-{
-    console.log('Generating the Token');
+test('Verifying E2E testing of CRUD operations', async ({ request }) => {
+    console.log('\nGenerating the Token');
 
-    const tokenRequestBody=structuredClone(tokenSourceRequestBody);
+    const tokenRequestBody = structuredClone(tokenSourceRequestBody);
 
-    const tokenResponse=await request.post('/auth',
+    const tokenResponse = await request.post('/auth',
         {
-            data:tokenRequestBody
+            data: tokenRequestBody
         }
     );
 
-    const tokenResponseBody=await tokenResponse.json();
+    const tokenResponseBody = await tokenResponse.json();
     console.log('The response of POST request is:');
     console.log(tokenResponseBody);
 
-    const tokenResult=tokenResponseBody.token;
+    console.log("Performing some assertions:");
+    expect(tokenResponse.ok()).toBeTruthy();
+    expect(tokenResponse.status()).toBe(200);
+
+    expect(tokenResponseBody).toHaveProperty('token');
+    console.log('All assertions successfully done.');
+
+    const tokenResult = tokenResponseBody.token;
     console.log(`Token ===> ${tokenResult}`);
 
-    console.log('Token successfully generated');
+    console.log('\nToken successfully generated');
 
-    console.log('POST request initiated');
+    console.log('\nPOST request initiated');
 
-    const postRequestBody=structuredClone(postSourceRequestBody);
+    const postRequestBody = structuredClone(postSourceRequestBody);
 
-    const postResponse=await request.post('/auth',
+    const postResponse = await request.post('/auth',
         {
-            data:postRequestBody
+            data: postRequestBody
         }
     );
 
-    const postResponseBody=await tokenResponse.json();
+    const postResponseBody = await tokenResponse.json();
     console.log('The response of POST request is:');
     console.log(postResponseBody);
 
-    const tokenResult=tokenResponseBody.token;
-    console.log(`Token ===> ${tokenResult}`);
+    console.log("Performing some assertions:");
+    expect(postResponse.ok()).toBeTruthy();
+    expect(postResponse.status()).toBe(200);
 
-    console.log('Token successfully generated');
+   // expect(postResponseBody).toHaveProperty('bookingid');
+    console.log('All assertions successfully done.');
+
+    const bookingID = postResponseBody.bookingid;
+    console.log(`Booking ID ===> ${bookingID}`);
+
+    console.log('\nPOST request successfully done');
 
 
 })
