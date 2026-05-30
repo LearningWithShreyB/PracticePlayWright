093fe34fdf3adb88d87eec701ad85595e9e9c5ff
ShreyB <shreypractice.github@gmail.com>
2026-05-30 13:52:53 +0530

    Updates_3005_13:53

--- tests/PWPractice290501.spec.ts
+++ tests/PWPractice290501.spec.ts
@@ -12,4 +12,26 @@ test('Verifying the TextBox functionality',async({page})=>
     await expect(textBox).toBeVisible();
 
     await expect(textBox).toBeEnabled();
+
+    await expect(textBox).toBeEditable();
+
+    await expect(textBox).toHaveAttribute('maxlength','15');
+
+    /* await textBox.fill('Playwright');
+    await expect(textBox).toHaveValue('Playwright');
+
+    await textBox.fill('Typescript');
+
+    const value=await textBox.inputValue();
+    expect(value).toBe('Typescript'); */
+
+    await textBox.fill('Playwright');
+    await expect(textBox).toHaveValue('Playwright');
+
+    //await textBox.fill('Typescript');
+
+    const value=await textBox.inputValue();
+    expect(value).toBe('Playwright');
+
+    
 })
