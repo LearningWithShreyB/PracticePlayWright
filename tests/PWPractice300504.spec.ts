7a7fd10fce28d95f845a8dea38caa4cf68aa8857
ShreyB <shreypractice.github@gmail.com>
2026-05-31 14:28:31 +0530

    Updates_3105_14:29

--- tests/PWPractice300504.spec.ts
+++ tests/PWPractice300504.spec.ts
@@ -3,23 +3,27 @@ import { test, expect, Locator } from '@playwright/test';
 test('Verifying the checkbox functionality - Part2', async ({ page }) => {
     await page.goto("https://testautomationpractice.blogspot.com/");
 
-    const days:Array<string>=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const checkboxes:Array<Locator>=days.map(day=>page.getByLabel(day));
+    const days: Array<string> = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const checkboxes: Array<Locator> = days.map(day => page.getByLabel(day));
     expect(checkboxes.length).toBe(days.length);
 
     await checkboxes[2].check();
     await expect(checkboxes[2]).toBeChecked();
     await page.waitForTimeout(2000);
 
-    for(const checkbox of checkboxes)
-    {
+    for (const checkbox of checkboxes) {
         await checkbox.check();
         await expect(checkbox).toBeChecked();
         await page.waitForTimeout(1000);
     }
     await page.waitForTimeout(2000);
 
-    for
+    for (const checkbox of checkboxes.slice(-2)) {
+        await checkbox.uncheck();
+        await expect(checkbox).not.toBeChecked();
+        await page.waitForTimeout(1000);
+    }
+    await page.waitForTimeout(2000);
 
 
 
