fb0ed587af74b3d28866b4e5d0e4c9c116e4de5c
ShreyB <shreypractice.github@gmail.com>
2026-05-31 14:25:58 +0530

    Updates_3105_14:26

--- tests/PWPractice300504.spec.ts
+++ tests/PWPractice300504.spec.ts
@@ -5,11 +5,22 @@ test('Verifying the checkbox functionality - Part2', async ({ page }) => {
 
     const days:Array<string>=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const checkboxes:Array<Locator>=days.map(day=>page.getByLabel(day));
+    expect(checkboxes.length).toBe(days.length);
 
     await checkboxes[2].check();
     await expect(checkboxes[2]).toBeChecked();
     await page.waitForTimeout(2000);
 
-    
+    for(const checkbox of checkboxes)
+    {
+        await checkbox.check();
+        await expect(checkbox).toBeChecked();
+        await page.waitForTimeout(1000);
+    }
+    await page.waitForTimeout(2000);
+
+    for
+
+
 
 });
