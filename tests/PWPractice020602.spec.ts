ff1da4f6155a399c8b740290b9dcd83a258b54bc
ShreyB <shreypractice.github@gmail.com>
2026-06-02 23:16:06 +0530

    Updates_0106_23:17

--- tests/PWPractice020602.spec.ts
+++ tests/PWPractice020602.spec.ts
@@ -2,8 +2,14 @@ import { test, expect } from '@playwright/test';
 
 test('Verifying duplicate elements of dropdown', async ({ page }) => {
 
+    await page.goto("https://testautomationpractice.blogspot.com/");
+
     const ddOptions: string[] = (await page.locator("#colors>option").allTextContents()).map(text => text.trim());
 
+    for(const i of ddOptions)
+    {
+        console.log(i);
+    }
     const mySet = new Set<string>;
     const duplicates: Array<string> = [];
 
@@ -18,4 +24,12 @@ test('Verifying duplicate elements of dropdown', async ({ page }) => {
 
     console.log('Elements containing in MySet', mySet);
     console.log('Elements having duplicates', duplicates);
+
+    if(duplicates.length>0)
+    {
+        console.log("Duplicate options found.", duplicates)
+    }
+    else{
+        console.log("No duplicate options found..")
+    }
 });
