0eb3c1578e61ce4728b0d0c6c6d991751d0dc6a6
ShreyB <shreypractice.github@gmail.com>
2026-06-04 00:22:43 +0530

    Updates_0406_00:21

--- tests/PWPractice030601.spec.ts
+++ tests/PWPractice030601.spec.ts
@@ -1,9 +1,15 @@
-import {test,expect} from '@playwright/test';
+import { test, expect, Locator } from '@playwright/test';
 
-test('Verifying the dropdown - Part2'async({page})=>
-{
+test('Verifying the dropdown - Part2', async ({ page }) => {
     await page.goto("https://www.bstackdemo.com/");
-    const dropdown01=page.locator("div.sort>select");
+    const dropdown01 = page.locator("div.sort>select");
     await expect(dropdown01).toBeVisible();
     await expect(dropdown01).toBeEnabled();
-})
+
+    await dropdown01.selectOption('Lowest to highest');
+
+    const name:Locator=page.locator("p.shelf-item__title");
+    const price:Locator=page.locator('div.val');
+
+    console.log("Name of the elements:",await name.allInnerTexts());
+});
