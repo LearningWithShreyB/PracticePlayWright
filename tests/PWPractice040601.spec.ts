38179030ecc8e6b725ed5aec9b82fee54c226bf1
ShreyB <shreypractice.github@gmail.com>
2026-06-04 19:33:26 +0530

    Updates_0406_19:34

--- tests/PWPractice040601.spec.ts
+++ tests/PWPractice040601.spec.ts
@@ -6,4 +6,16 @@ test('Verifying the Auto suggested dropdown',async({page})=>
 
     await page.locator("#APjFqb").fill("playwright");
     await page.waitForTimeout(2000);
+
+    const options:Locator=page.locator("#Alh6id");
+
+    const count:number= await options.count();
+    console.log("No of suggested options are:",count);
+
+    console.log("Printing all the auto suggestions.....")
+    for(let i=0;i<count;i++)
+    {
+        //console.log(await options.nth(i).innerText());
+        console.log(await options.nth(i).textContent());
+    }
 });
