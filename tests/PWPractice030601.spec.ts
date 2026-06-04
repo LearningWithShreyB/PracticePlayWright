805d8d5daef835438084ac1991fd7280345c32f2
ShreyB <shreypractice.github@gmail.com>
2026-06-04 18:01:20 +0530

    Updates_0406_18:03

--- tests/PWPractice030601.spec.ts
+++ tests/PWPractice030601.spec.ts
@@ -8,8 +8,23 @@ test('Verifying the dropdown - Part2', async ({ page }) => {
 
     await dropdown01.selectOption('Lowest to highest');
 
-    const name:Locator=page.locator("p.shelf-item__title");
-    const price:Locator=page.locator('div.val');
+    const name: Locator = page.locator("p.shelf-item__title");
+    const price: Locator = page.locator('div.val');
 
-    console.log("Name of the elements:",await name.allInnerTexts());
+    console.log("The name of all the elements:", await name.allInnerTexts());
+    console.log("The price of all the elements:", await price.allTextContents());
+
+    const nameString: string[] = await name.allTextContents();
+    const priceString: Array<string> = await price.allInnerTexts();
+
+    console.log("The name of all the elements:", nameString);
+    console.log("The price of all the elements:", priceString);
+
+    expect(nameString.length).toBe(priceString.length);
+
+    for (let i = 0; i < priceString.length; i++) {
+        console.log(`${nameString[i]} : ${priceString[i]}`);
+    }
+
+
 });
