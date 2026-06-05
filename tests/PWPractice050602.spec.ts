e69b97d486efdee82c87a31bc6962b7693155e01
ShreyB <shreypractice.github@gmail.com>
2026-06-05 18:46:02 +0530

    Updates_0506_18:46

--- tests/PWPractice050602.spec.ts
+++ tests/PWPractice050602.spec.ts
@@ -1,14 +1,16 @@
 import { test, expect, Locator } from '@playwright/test';
 
 test('Verifying auto-suggested dropdown of Amazon', async ({ page }) => {
-    await page.goto("https://www.amazon.in/");
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
     await page.locator("input[placeholder='Username']").fill('Admin');
     await page.locator("input[placeholder='Password']").fill('admin123');
     await page.locator("button[type='submit']").click();
 
-    const options: Locator = page.locator("div[role='rowgroup'] div[role='button']");
-    await expect(options.first()).toBeVisible({ timeout: 5000 });
+    await page.getByText('PIM').click();
+
+    const options: Locator = page.locator("div.oxd-select-text-input");
+    await options.nth(2).click();
 
     const count: number = await options.count();
 
@@ -18,7 +20,7 @@ test('Verifying auto-suggested dropdown of Amazon', async ({ page }) => {
         console.log(`Dropdown Option [${i + 1}]: ${text}`);
     }
 
-    for (let i = 0; i < count; i++) {
+    /* for (let i = 0; i < count; i++) {
         const optionLocator = options.nth(i);
         const text = await optionLocator.innerText();
         console.log(`Dropdown Option [${i + 1}]: ${text}`);
@@ -26,5 +28,5 @@ test('Verifying auto-suggested dropdown of Amazon', async ({ page }) => {
             await optionLocator.click();
             break;
         }
-    }
+    } */
 });
