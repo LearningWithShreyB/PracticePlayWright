5269a9402d86e6c0f7921e629570c0359bf3b27b
ShreyB <shreypractice.github@gmail.com>
2026-06-09 23:19:17 +0530

    Updates_0906_23:20

--- tests/PWPractice090601.spec.ts
+++ tests/PWPractice090601.spec.ts
@@ -7,8 +7,28 @@ test('Verifying static table',async({page})=>{
 
     console.log("Counting the number of rows:");
     const tableRows:Locator=table.locator("tr");
-    const count:number=await tableRows.count();
-    console.log("The number of rows are:",count);
+    const rowsCount:number=await tableRows.count();
+    console.log("The number of rows are:",rowsCount);
+    await expect(tableRows).toHaveCount(rowsCount);
+
+    console.log("Counting the number of headers and columns");
+    const tableColumns:Locator=table.locator("th");
+    const columnCount:number=await tableColumns.count();
+    console.log("The number of columns are:",columnCount);
+    expect(columnCount).toBe(4);
+
+    console.log("Reading all data from 3rd row excluding headers");
+    const thirdRow:Locator=tableRows.nth(3).locator("td")
+    const thirdRowData:string[]=await thirdRow.allInnerTexts();
+    //console.log(thirdRowData);
+
+    for(let data of thirdRowData)
+    {
+        console.log(data);
+    }
+
+   
+    
 
 
-})
+});
