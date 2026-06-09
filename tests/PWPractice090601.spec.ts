a2c383387c5d926bbae295aad75dd1431b834b80
ShreyB <shreypractice.github@gmail.com>
2026-06-09 23:38:34 +0530

    Updates_0906_23:40

--- tests/PWPractice090601.spec.ts
+++ tests/PWPractice090601.spec.ts
@@ -5,19 +5,19 @@ test('Verifying static table',async({page})=>{
     const table:Locator=page.locator("table[name='BookTable']");
     await expect(table).toBeVisible();
 
-    console.log("Counting the number of rows:");
+    console.log("\nCounting the number of rows:");
     const tableRows:Locator=table.locator("tr");
     const rowsCount:number=await tableRows.count();
     console.log("The number of rows are:",rowsCount);
     await expect(tableRows).toHaveCount(rowsCount);
 
-    console.log("Counting the number of headers and columns");
+    console.log("\nCounting the number of headers and columns");
     const tableColumns:Locator=table.locator("th");
     const columnCount:number=await tableColumns.count();
     console.log("The number of columns are:",columnCount);
     expect(columnCount).toBe(4);
 
-    console.log("Reading all data from 3rd row excluding headers");
+    console.log("\nReading all data from 3rd row excluding headers");
     const thirdRow:Locator=tableRows.nth(3).locator("td")
     const thirdRowData:string[]=await thirdRow.allInnerTexts();
     //console.log(thirdRowData);
@@ -28,7 +28,15 @@ test('Verifying static table',async({page})=>{
     }
 
    
-    
+    console.log("\nRead all data from the table (excluding header)");
+    const allRowsData:Locator[]=await tableRows.all();
+
+    const rowData=allRowsData.slice(1);
+
+    for(let data of rowData){
+        const dataRows:string[]=await data.locator("td").allInnerTexts();
+        console.log(dataRows.join(" | "))
+    }
 
 
 });
