8b44533e05b736a115ba2edf1108c00255281fd3
ShreyB <shreypractice.github@gmail.com>
2026-06-09 22:43:49 +0530

    Updates_0906_22:44

--- tests/PWPractice090601.spec.ts
+++ tests/PWPractice090601.spec.ts
@@ -5,4 +5,10 @@ test('Verifying static table',async({page})=>{
     const table:Locator=page.locator("table[name='BookTable']");
     await expect(table).toBeVisible();
 
+    console.log("Counting the number of rows:");
+    const tableRows:Locator=table.locator("tr");
+    const count:number=await tableRows.count();
+    console.log("The number of rows are:",count);
+
+
 })
