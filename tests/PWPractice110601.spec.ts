08c11db0bcdeb7316eecc73df66a889b2aec7f39
ShreyB <shreypractice.github@gmail.com>
2026-06-11 00:39:19 +0530

    Updates_1106_00:39

--- tests/PWPractice110601.spec.ts
+++ tests/PWPractice110601.spec.ts
@@ -1,14 +1,35 @@
-import {test,expect, Locator} from '@playwright/test';
+import { test, expect, Locator } from '@playwright/test';
 
-test('Verifying the Dynamic Table', async({page})=>
-{
+test('Verifying the Dynamic Table', async ({ page }) => {
     await page.goto("https://testautomationpractice.blogspot.com/");
-    const table:Locator=page.locator('#taskTable tbody');
+    const table: Locator = page.locator('#taskTable tbody');
     await expect(table).toBeVisible();
 
-    const tableRows:Locator[]=await table.locator("tr").all();
+    const tableRows: Locator[] = await table.locator("tr").all();
     expect(tableRows).toHaveLength(4);
 
-    
+    let cpuLoad = '';
 
-})
+    for (let row of tableRows) {
+        const name: string = await row.locator('td').first().innerText();
+
+        if (name === 'Chrome') {
+            cpuLoad = await row.locator("td:has-text('%')").innerText();
+            console.log('The CPU Load of chrome is:', cpuLoad);
+            break;
+        }
+    }
+
+    const cpuLoadText: string = await page.locator(".chrome-cpu").innerText();
+
+    if (cpuLoadText === cpuLoad) {
+        console.log("Good Job!!");
+    }
+    else {
+        console.log("Try again!!");
+    }
+
+    expect(cpuLoadText).toBe(cpuLoad);
+
+
+});
