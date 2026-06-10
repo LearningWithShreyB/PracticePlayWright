8fb63316088badb0802610659aeccd05157152c0
ShreyB <shreypractice.github@gmail.com>
2026-06-11 00:05:07 +0530

    Updates_1106_00:06

--- tests/PWPractice100601.spec.ts
+++ tests/PWPractice100601.spec.ts
@@ -10,7 +10,7 @@ test('Verifying the Dynamic Table', async ({ page }) => {
     console.log("The number of rows:", tableRows.length);
     expect(tableRows).toHaveLength(4);
 
-    let cpuLoad;
+    let cpuLoad = '';
     for (let row of tableRows) {
         const name: string = await row.locator("td").first().innerText();
         if (name === 'Chrome') {
@@ -19,5 +19,19 @@ test('Verifying the Dynamic Table', async ({ page }) => {
             break;
         }
     }
+
+    const textBox: string = await page.locator("#chrome-cpu").innerText();
+    console.log("The text written in yellow text bob is:", textBox);
+
+    if (textBox.includes(cpuLoad)) {
+        console.log("Good job!!");
+    }
+    else {
+        console.log("Try again!!");
+    }
+
+    expect(textBox).toContain(cpuLoad);
+
+    await page.waitForTimeout(3000);
 
 });
