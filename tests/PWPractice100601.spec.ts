7516f339532b0828b3170eb8b9e66e95afcee7b2
ShreyB <shreypractice.github@gmail.com>
2026-06-10 23:57:09 +0530

    Updates_1006_23:58

--- tests/PWPractice100601.spec.ts
+++ tests/PWPractice100601.spec.ts
@@ -1,9 +1,23 @@
-import {test,expect, Locator} from "@playwright/test";
+import { test, expect, Locator } from "@playwright/test";
 
-test('Verifying the Dynamic Table',async({page})=>{
+test('Verifying the Dynamic Table', async ({ page }) => {
     await page.goto("https://practice.expandtesting.com/dynamic-table");
 
-    const table:Locator= page.locator(".table tbody");
+    const table: Locator = page.locator(".table tbody");
     await expect(table).toBeVisible();
 
-})
+    const tableRows: Locator[] = await table.locator('tr').all();
+    console.log("The number of rows:", tableRows.length);
+    expect(tableRows).toHaveLength(4);
+
+    let cpuLoad;
+    for (let row of tableRows) {
+        const name: string = await row.locator("td").first().innerText();
+        if (name === 'Chrome') {
+            cpuLoad = await row.locator("td", { hasText: '%' }).innerText();
+            console.log("The value of CPU Load is:", cpuLoad);
+            break;
+        }
+    }
+
+});
