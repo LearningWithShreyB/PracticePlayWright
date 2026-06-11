99a1cdc33542c1a18dc7d0c43476744145d59f00
ShreyB <shreypractice.github@gmail.com>
2026-06-11 18:37:50 +0530

    Updates_1106_18:38

--- tests/PWPractice110604.spec.ts
+++ tests/PWPractice110604.spec.ts
@@ -1,32 +1,39 @@
-import {test,expect} from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-test('Verifying the pagination table',async({page})=>
-{
+test('Verifying the pagination table', async ({ page }) => {
     await page.goto("https://datatables.net/examples/basic_init/zero_configuration.html");
 
     /* const tableRows=await page.locator(".display tbody tr").all();
     expect(tableRows).toHaveLength(10); */
 
-    let hasMorePages=true;
+    let hasMorePages = true;
+    let totalRecords = 0;
 
-    while(hasMorePages){
-        const tableRows=await page.locator(".display tbody tr").all();
-        for(let row of tableRows){
+    while (hasMorePages) {
+        const tableRows = await page.locator(".display tbody tr").all();
+        totalRecords += tableRows.length;
+        for (let row of tableRows) {
             const cellTexts = await row.locator('td').allInnerTexts();
             console.log(cellTexts.join(' | '));
         }
 
-        const next=page.locator("button[aria-label='Next']");
-        const isDisabled=await next.getAttribute('class');
+        const next = page.locator("button[aria-label='Next']");
+        const isDisabled = await next.getAttribute('class');
 
         await page.waitForTimeout(3000);
 
-        if(isDisabled?.includes('disabled')){
-            hasMorePages=false;
+        if (isDisabled?.includes('disabled')) {
+            hasMorePages = false;
         }
-        else{
+        else {
             await next.click();
         }
     }
+
+    console.log(`The total number of records are ${totalRecords}`);
+
+    const label = await page.locator("div[aria-live='polite']").innerText();
+
+    expect(label).toContain(String(totalRecords));
 
 });
