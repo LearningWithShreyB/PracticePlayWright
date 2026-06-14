db0dd2502edb350971bf54b34cc89b396efe8784
ShreyB <shreypractice.github@gmail.com>
2026-06-14 19:08:47 +0530

    Updates_1406_19:09

--- tests/PWDatePickerPractice140607.spec.ts
+++ tests/PWDatePickerPractice140607.spec.ts
@@ -1,19 +1,53 @@
-import {test,expect,Locator} from '@playwright/test';
+import { test, expect, Locator } from '@playwright/test';
 
-test('Verifying the Date Picker Part-06',async ({page})=>{
-    await page.goto("https://www.booking.com/");
+async function selectDate(targetMonth: string, targetDate: string, targetYear: string, page: Page,) {
 
-    await page.locator("button[aria-label='Select dates']").click();
-    //await page.getByTestId('searchbox-dates-container').click();
+    const monthDropdown = page.locator("select.ui-datepicker-month");
+    await monthDropdown.selectOption({ label: targetMonth });
+    const yearDropdown = page.locator("select.ui-datepicker-year");
+    await yearDropdown.selectOption({ label: targetYear });
 
-    await page.waitForTimeout(5000);
+    const allDate = await page.locator(".ui-datepicker-calendar td").all();
 
-    const checkInDate='28';
-    const checkInMonth='August';
-    const checkInYear='2027';
+    for (let date1 of allDate) {
+        const dateText = await date1.innerText();
 
-    while(true)
-{
-
+        if (dateText === targetDate) {
+            await date1.click();
+            break;
+        }
+    }
 }
+
+
+test('Verifying the Date Picker Part-06', async ({ page }) => {
+    await page.goto("https://www.dummyticket.com/dummy-ticket-for-visa-application/");
+
+    await expect(page).toHaveTitle(/Dummy ticket/);
+
+    const radio01 = page.locator("#product_549");
+    await radio01.check();
+    await expect(radio01).toBeChecked();
+
+    await page.locator("#travname").fill("Akash");
+    await page.locator("#travlastname").fill("Rathod");
+
+    const datePicker = page.locator("#dob");
+    await datePicker.click();
+
+    const month = 'Apr';
+    const year = '1999';
+    const date = '26';
+
+    await selectDate(month, date, year, page);
+
+    await expect(datePicker).toHaveValue("26/04/1999");
+
+    const radio02 = page.locator("#sex_1");
+    await radio02.check();
+    await expect(radio01).toBeChecked();
+
+    await page.waitForTimeout(3000);
+
+
 });
