0ffa1c5d932a3d6568e28c86f3d2dfdec66c78e7
ShreyB <shreypractice.github@gmail.com>
2026-06-15 18:38:33 +0530

    Updates_1506_18:39

--- tests/PWFramesPractice150601.spec.ts
+++ tests/PWFramesPractice150601.spec.ts
@@ -5,14 +5,28 @@ test('Verifying Frames-Part01', async ({ page }) => {
     const frames = page.frames();
     console.log("The number of frames are:", frames.length);
 
-    const text = page.locator("input[name='mytext3']");
+    const frame3=page.frame({url:'https://ui.vision/demo/webtest/frames/frame_3.html'});
+    
+      if(frame3){
+          await frame3.locator("[name='mytext3']").fill("Welcome");
+           const childFrames= frame3.childFrames();
+           console.log("Child frames inside the Frame 3:", childFrames.length); // only 1 child frame exist
+           const radio=childFrames[0].getByLabel("I am a human");
+           await radio.check(); // select radio button
+            await expect(radio).toBeChecked();// assertion
+       }
+        else{
+            console.log("Frame 3 is not found..")
+        }
+
+    /* const text = page.locator("input[name='mytext3']");
     text.fill("Welcome!!");
     expect(text).toHaveAttribute('type');
 
 
     /* const radio01=page.getByLabel('I am a human');
     await radio01.check();
-    await expect(radio01).toBeChecked(); */
+    await expect(radio01).toBeChecked(); 
 
     const frame = page.frame({ url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLScPXRMtYI_KYL8J6fivHUV0hQKB7j1RtqTrBBUtEr8VMmyCqw/formResponse?pli=1&embedded=true" });
     if (frame) {
@@ -22,7 +36,7 @@ test('Verifying Frames-Part01', async ({ page }) => {
     }
     else {
         console.log("Frame is not available");
-    }
+    } */
 
     await page.waitForTimeout(3000);
 });
