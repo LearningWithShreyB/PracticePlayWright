18c9acbc924ac20555654b75d04fb84fa999b906
ShreyB <shreypractice.github@gmail.com>
2026-06-15 18:29:19 +0530

    Updates_1506_18:30

---
+++ tests/PWFramesPractice150601.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+
+test('Verifying Frames-Part01', async ({ page }) => {
+    await page.goto("https://ui.vision/demo/webtest/frames/frame_3");
+    const frames = page.frames();
+    console.log("The number of frames are:", frames.length);
+
+    const text = page.locator("input[name='mytext3']");
+    text.fill("Welcome!!");
+    expect(text).toHaveAttribute('type');
+
+
+    /* const radio01=page.getByLabel('I am a human');
+    await radio01.check();
+    await expect(radio01).toBeChecked(); */
+
+    const frame = page.frame({ url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLScPXRMtYI_KYL8J6fivHUV0hQKB7j1RtqTrBBUtEr8VMmyCqw/formResponse?pli=1&embedded=true" });
+    if (frame) {
+        const radio01 = frame.getByLabel('I am a human');
+        await radio01.check();
+        await expect(radio01).toBeChecked();
+    }
+    else {
+        console.log("Frame is not available");
+    }
+
+    await page.waitForTimeout(3000);
+});
