38e9457ae6d06c244ac21387ec7480548b7ef28b
ShreyB <shreypractice.github@gmail.com>
2026-06-15 22:44:34 +0530

    Updates_1506_22:45

--- tests/PWFramesPractice150601.spec.ts
+++ tests/PWFramesPractice150601.spec.ts
@@ -9,46 +9,16 @@ test('Verifying Frames-Part01', async ({ page }) => {
     text.fill("Welcome!!");
     expect(text).toHaveAttribute('type');
 
-    const frame=page.frame({url:"https://docs.google.com/forms/d/1k8PNZrtmCzZi6mW6z-9k4_h4ZSFakJLWseITLyAwp9k/viewform?embedded=true"})
+    const frame = page.frame({ url: /.*docs.google.com\/forms.*/ })
 
-    if(frame){
-        await frame.getByLabel('I am a human').check();
-    }
-    else{
-        console.log("No frame is available..");
-    }
-    /* const frame3=page.frame({url:'https://ui.vision/demo/webtest/frames/frame_3.html'});
-    
-      if(frame3){
-          await frame3.locator("[name='mytext3']").fill("Welcome");
-           const childFrames= frame3.childFrames();
-           console.log("Child frames inside the Frame 3:", childFrames.length); // only 1 child frame exist
-           const radio=childFrames[0].getByLabel("I am a human");
-           await radio.check(); // select radio button
-            await expect(radio).toBeChecked();// assertion
-       }
-        else{
-            console.log("Frame 3 is not found..")
-        } */
-
-    /* const text = page.locator("input[name='mytext3']");
-    text.fill("Welcome!!");
-    expect(text).toHaveAttribute('type');
-
-
-    /* const radio01=page.getByLabel('I am a human');
-    await radio01.check();
-    await expect(radio01).toBeChecked(); 
-
-    const frame = page.frame({ url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLScPXRMtYI_KYL8J6fivHUV0hQKB7j1RtqTrBBUtEr8VMmyCqw/formResponse?pli=1&embedded=true" });
     if (frame) {
-        const radio01 = frame.getByLabel('I am a human');
-        await radio01.check();
-        await expect(radio01).toBeChecked();
+        console.log("Google Form frame found successfully!");
+        await frame.getByLabel('I am a human').check();
+        await expect(frame.getByLabel('I am a human')).toBeChecked();
     }
     else {
-        console.log("Frame is not available");
-    } */
+        console.log("No frame is available..");
+    }
 
     await page.waitForTimeout(3000);
 });
