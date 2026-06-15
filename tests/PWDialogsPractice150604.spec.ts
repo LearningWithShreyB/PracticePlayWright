f9b1e25b79a5a9daa588788abeb2852cd6488890
ShreyB <shreypractice.github@gmail.com>
2026-06-15 13:30:47 +0530

    Updates_1506_13:31

---
+++ tests/PWDialogsPractice150604.spec.ts
@@ -0,0 +1,24 @@
+import {test,expect} from '@playwright/test';
+
+test('Verifying the Prompt Dialog for Default Value',async({page})=>
+{
+    await page.goto('https://testautomationpractice.blogspot.com/');
+
+    page.on('dialog',(dialog)=>{
+        console.log("Dialog type is:",dialog.type());
+        expect(dialog.type()).toContain('prompt');
+        console.log("The dialog text is:",dialog.message());
+        expect(dialog.message()).toContain("Please enter your name:");
+        console.log("The default value of dialog box is",dialog.defaultValue());
+        expect(dialog.defaultValue()).toBe("Harry Potter");
+        dialog.accept("John");
+    });
+
+
+    await page.locator("#promptBtn").click();
+    const text=await page.locator("#demo").innerText();
+    console.log("The text is:",text);
+   expect(page.locator("#demo")).toContainText("John");
+
+    await page.waitForTimeout(3000);
+});
