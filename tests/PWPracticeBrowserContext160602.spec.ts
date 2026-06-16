87dc4dc21f4cd5428d913fd7b474b9b2a979e530
ShreyB <shreypractice.github@gmail.com>
2026-06-16 19:32:54 +0530

    Updates_1606_19:33

---
+++ tests/PWPracticeBrowserContext160602.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect, chromium, firefox, webkit, Page } from '@playwright/test';
+
+test('Testing Browser Context', async () => {
+
+    const browser = await firefox.launch();
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    await page.goto('https://playwright.dev/');
+    await expect(page).toHaveTitle(/Playwright/);
+    await page.waitForTimeout(3000);
+
+});
