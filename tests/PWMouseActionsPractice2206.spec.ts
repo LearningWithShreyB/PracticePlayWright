0847a548ae7d2ad912843a3e005c2a4f7c1dc18d
ShreyB <shreypractice.github@gmail.com>
2026-06-22 13:17:53 +0530

    Updates_2206_13:18

--- tests/PWMouseActionsPractice2206.spec.ts
+++ tests/PWMouseActionsPractice2206.spec.ts
@@ -1,38 +1,57 @@
-import {test,expect, Locator} from '@playwright/test';
+import { test, expect, Locator } from '@playwright/test';
 
-test('Verifying Mouse Actions-Mouse Hover',async({page})=>{
+test('Verifying Mouse Actions-Mouse Hover', async ({ page }) => {
     await page.goto("https://testautomationpractice.blogspot.com/");
 
-    const btn01:Locator= page.locator(".dropbtn");
+    const btn01: Locator = page.locator(".dropbtn");
     await btn01.hover();
     await expect(btn01).toBeVisible();
     await page.waitForTimeout(2000);
 
-    const btn02:Locator= page.getByRole('link',{name:'Mobiles'});
+    const btn02: Locator = page.getByRole('link', { name: 'Mobiles' });
     await btn02.hover();
     await expect(btn02).toBeVisible();
     await page.waitForTimeout(2000);
 
 });
 
-test('Verifying Mouse Actions-Right click',async({page})=>{
+test('Verifying Mouse Actions-Right click', async ({ page }) => {
     await page.goto("http://swisnl.github.io/jQuery-contextMenu/demo.html");
 
-    const btn03:Locator= page.locator(".context-menu-one");
-    await btn03.click({button:'right'});
+    const btn03: Locator = page.locator(".context-menu-one");
+    await btn03.click({ button: 'right' });
     await expect(btn03).toHaveText('right click me');
     await page.waitForTimeout(2000);
 
 });
 
-test.only('Verifying Mouse Actions-Double click',async({page})=>{
+test('Verifying Mouse Actions-Double click', async ({ page }) => {
     await page.goto("https://testautomationpractice.blogspot.com/");
 
-    const btn04:Locator= page.getByText("Copy Text");
+    const btn04: Locator = page.getByText("Copy Text");
     await btn04.dblclick();
     await expect(btn04).toBeEnabled();
-    const text=page.locator("#field2");
+    const text = page.locator("#field2");
     await expect(text).toHaveValue("Hello World!");
+    await page.waitForTimeout(2000);
+
+});
+
+test.only('Verifying Mouse Actions-Drag & Drop', async ({ page }) => {
+    await page.goto("https://testautomationpractice.blogspot.com/");
+
+    const source = page.locator('#draggable');
+    const target = page.locator("#droppable");
+
+    await expect(target).toHaveText('Drop here');
+
+    await source.hover();
+    await page.mouse.down();
+    await target.hover();
+    await page.mouse.up();
+
+    await expect(target).toHaveText('Dropped!');
+
     await page.waitForTimeout(2000);
 
 });
